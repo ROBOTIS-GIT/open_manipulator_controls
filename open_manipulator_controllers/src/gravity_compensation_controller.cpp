a9f06821d389c6d7c727b5c6c2fd719c84c067c7
Ryan Shim <jhshim@robotis.com>
2020-05-11 16:33:00 +0900

    linted

--- open_manipulator_controllers/src/gravity_compensation_controller.cpp
+++ open_manipulator_controllers/src/gravity_compensation_controller.cpp
@@ -18,14 +18,16 @@
 
 #include "open_manipulator_controllers/gravity_compensation_controller.h"
 
-namespace open_manipulator_controllers {
-
+namespace open_manipulator_controllers
+{
 bool GravityCompensationController::init(
-    hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) {
+    hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle)
+{
   // Joint interface
   effort_joint_interface_ =
       robot_hardware->get<hardware_interface::EffortJointInterface>();
-  if (effort_joint_interface_ == nullptr) {
+  if (effort_joint_interface_ == nullptr)
+  {
     ROS_ERROR(
         "[GravityCompensationController] Could not get effort joint interface "
         "from hardware!");
@@ -33,15 +35,20 @@ bool GravityCompensationController::init(
   }
 
   // Joint handle
-  if (!node_handle.getParam("joint_names", joint_names_)) {
+  if (!node_handle.getParam("joint_names", joint_names_))
+  {
     ROS_ERROR("[GravityCompensationController] Could not parse joint names");
     return false;
   }
-  for (size_t i = 0; i < joint_names_.size(); i++) {
-    try {
+  for (size_t i = 0; i < joint_names_.size(); i++)
+  {
+    try
+    {
       effort_joint_handles_.push_back(
           effort_joint_interface_->getHandle(joint_names_[i]));
-    } catch (const hardware_interface::HardwareInterfaceException& e) {
+    }
+    catch (const hardware_interface::HardwareInterfaceException& e)
+    {
       ROS_ERROR_STREAM(
           "[GravityCompensationController] Could not get joint handle: "
           << e.what());
@@ -51,24 +58,28 @@ bool GravityCompensationController::init(
 
   // KDL
   urdf::Model urdf;
-  if (!urdf.initParam("robot_description")) {
+  if (!urdf.initParam("robot_description"))
+  {
     ROS_ERROR("[GravityCompensationController] Could not parse urdf file");
     return false;
   }
-  if (!kdl_parser::treeFromUrdfModel(urdf, kdl_tree_)) {
+  if (!kdl_parser::treeFromUrdfModel(urdf, kdl_tree_))
+  {
     ROS_ERROR("[GravityCompensationController] Could not construct kdl tree");
     return false;
   }
-  std::string root_name, tip_name;
-  if (!node_handle.getParam("root_link", root_name)) {
+  if (!node_handle.getParam("root_link", root_name_))
+  {
     ROS_ERROR("[GravityCompensationController] Could not find root link name");
     return false;
   }
-  if (!node_handle.getParam("tip_link", tip_name)) {
+  if (!node_handle.getParam("tip_link", tip_name_))
+  {
     ROS_ERROR("[GravityCompensationController] Could not find tip link name");
     return false;
   }
-  if (!kdl_tree_.getChain(root_name, tip_name, kdl_chain_)) {
+  if (!kdl_tree_.getChain(root_name, tip_name, kdl_chain_))
+  {
     ROS_ERROR(
         "[GravityCompensationController] Could not get KDL chain from tree");
     return false;
@@ -90,22 +101,26 @@ bool GravityCompensationController::init(
 void GravityCompensationController::starting(const ros::Time& time) {}
 
 void GravityCompensationController::update(const ros::Time& time,
-                                           const ros::Duration& period) {
+                                           const ros::Duration& period)
+{
   // Get the current joint positions
-  for (size_t i = 0; i < kdl_chain_.getNrOfJoints(); i++) {
+  for (size_t i = 0; i < kdl_chain_.getNrOfJoints(); i++)
+  {
     q_(i) = effort_joint_handles_[i].getPosition();
   }
 
   // Compute the gravity torque
   MCG_solver_->JntToGravity(q_, G_);
 
-  for (size_t i = 0; i < kdl_chain_.getNrOfJoints(); i++) {
+  for (size_t i = 0; i < kdl_chain_.getNrOfJoints(); i++)
+  {
     tau_(i) = 0;
     tau_(i) += G_(i);
   }
 
   // Set effort command
-  for (size_t i = 0; i < kdl_chain_.getNrOfJoints(); i++) {
+  for (size_t i = 0; i < kdl_chain_.getNrOfJoints(); i++)
+  {
     effort_joint_handles_[i].setCommand(tau_(i));
   }
 }
